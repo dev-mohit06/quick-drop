0a4caee35ebca0ea43983a6a917606d247e25fc0
Mohit Paddhariya <dev.mohit2006@gmail.com>
2025-03-31 04:13:21 +0530

    cors fixes

--- server/utils/cors.util.js
+++ server/utils/cors.util.js
@@ -1,5 +1,7 @@
 const corsOption = {
-    origin : "*",
-}
+    origin: ["https://quick-drop-server-zeta.vercel.app/","http://localhost:3000"], // Change this to your actual frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, // If using cookies or authentication
+};
 
 module.exports = corsOption;
